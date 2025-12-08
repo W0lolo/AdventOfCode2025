756a99c6f63c12da0efe77c7931b682726eaf670
W0lolo <aaronseanagius@gmail.com>
2025-12-08 14:12:35 +0100

    Solved day 8!

--- src/src/Main.java
+++ src/src/Main.java
@@ -2,12 +2,24 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
+        /* // day 7
         String pathToInput = "Input/Day7.txt";
         String pathToInputTest = "Input/Day7test.txt";
         String inp = pathToInput;
         try{
             System.out.println("Part1 solution: "+Day7.day7SolverPart1(inp));
             System.out.println("Part2 solution: "+Day7.day7SolverPart2(inp));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+         */
+
+        String pathToInput = "Input/Day8.txt";
+        String pathToInputTest = "Input/Day8test.txt";
+        String inp = pathToInput;
+        try{
+            //System.out.println(Day8.day8SolverPart1(inp,1000));
+            System.out.println(Day8.day8SolverPart2(inp));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
